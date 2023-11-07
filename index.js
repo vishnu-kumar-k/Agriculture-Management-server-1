9f92be2ad55388a88ff6c60b309c44ec0ac65eb5
arunkumar-sivapragasam <jerryarun10@gmail.com>
2023-11-07 12:32:57 +0530

    l1

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000","https://mindverse.netlify.app"],
+    origin: ["http://localhost:3000"],
     credentials: true,
   })
 );
